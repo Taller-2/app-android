74a4574d702ef9ebe2b38f3457ce20a25b569910
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-09-07 18:41:43 -0300

    Add firebase auth dependencies

--- app/src/main/java/ar/uba/fi/mercadolibre/MainActivity.java
+++ app/src/main/java/ar/uba/fi/mercadolibre/MainActivity.java
@@ -3,11 +3,26 @@ package ar.uba.fi.mercadolibre;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            setContentView(R.layout.activity_signed_in);
+        }
     }
 }
