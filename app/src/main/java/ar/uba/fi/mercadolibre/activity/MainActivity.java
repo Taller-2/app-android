0fb35d37b952002393f227bd6571953299a7ed9a
Dylan Alvarez <helllo5d123@gmail.com>
2018-09-20 23:48:36 -0300

    Add user's token to all request headers

--- app/src/main/java/ar/uba/fi/mercadolibre/activity/MainActivity.java
+++ app/src/main/java/ar/uba/fi/mercadolibre/activity/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -15,6 +16,8 @@ import com.google.firebase.auth.GetTokenResult;
 
 import java.util.Arrays;
 import java.util.List;
+
+import ar.uba.fi.mercadolibre.client.RetrofitClient;
 
 public class MainActivity extends BaseActivity {
     private FirebaseAuth mAuth;
@@ -84,6 +87,13 @@ public class MainActivity extends BaseActivity {
 
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser != null) {
+            currentUser.getIdToken(true).addOnSuccessListener(
+                    new OnSuccessListener<GetTokenResult>() {
+                        @Override
+                        public void onSuccess(GetTokenResult result) {
+                            RetrofitClient.firebaseToken = result.getToken();
+                        }
+                    });
             startActivity(new Intent(getApplicationContext(), MainMenuActivity.class));
         } else {  // Firebase login view
             this.setFirebaseLoginActivity();
--- app/src/main/java/ar/uba/fi/mercadolibre/client/RetrofitClient.java
+++ app/src/main/java/ar/uba/fi/mercadolibre/client/RetrofitClient.java
@@ -1,16 +1,44 @@
 package ar.uba.fi.mercadolibre.client;
 
+import android.support.annotation.NonNull;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static Retrofit retrofit = null;
+    private static Retrofit.Builder retrofit = null;
+    public static String firebaseToken = null;
 
     public static Retrofit getClient(String url) {
-        if (retrofit != null) return retrofit;
-        retrofit = new Retrofit.Builder().baseUrl(url)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        return retrofit;
+        if (retrofit != null) return retrofit.build();
+        retrofit = new Retrofit
+                .Builder()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create());
+        if (firebaseToken != null) {
+            retrofit.client(httpClientBuilder().build());
+        }
+        return retrofit.build();
+    }
+
+    private static OkHttpClient.Builder httpClientBuilder() {
+        OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
+        httpClientBuilder.addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(@NonNull Chain chain) throws IOException {
+                return chain.proceed(
+                        chain.request().newBuilder().addHeader(
+                                "Authorization",
+                                firebaseToken
+                        ).build()
+                );
+            }
+        });
+        return httpClientBuilder;
     }
 }
